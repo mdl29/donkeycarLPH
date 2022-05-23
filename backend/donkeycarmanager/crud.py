b89f531e15e8e142cd473b72b18a6a2812a2140a
Benjamin BERNARD <benjamin.bernard@benvii.com>
2022-05-23 23:26:29 +0200

    :sparkles: Adding CORS (allow all origins) and move items / players in driving waiting queue.

--- backend/donkeycarmanager/crud.py
+++ backend/donkeycarmanager/crud.py
@@ -1,9 +1,12 @@
 from typing import Optional
 from sqlalchemy import asc, desc
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Session
 
 from donkeycarmanager import models, schemas
+from donkeycarmanager.database import engine
 
+RANKING_STEP = 1000  # How much place by default between 2 players in driving waiting queue
 
 def get_player(db: Session, player_id: int):
     return db.query(models.Player).filter(models.Player.player_id == player_id).first()
@@ -41,12 +44,11 @@ def get_driving_waiting_queue_by_rank(db: Session, skip: int = 0, limit: int = 1
         .limit(limit)\
         .all()
 
-
 def create_driving_waiting_queue(db: Session, driving_waiting_queue: schemas.DrivingWaitingQueueCreate):
     # Find ranking number
     last_driver: Optional[models.DrivingWaitingQueue] = db.query(models.DrivingWaitingQueue).\
         order_by(desc(models.DrivingWaitingQueue.rank)).limit(1).first()
-    new_rank = last_driver.rank + 1000 if last_driver else 1
+    new_rank = last_driver.rank + RANKING_STEP if last_driver else RANKING_STEP  # Don't start at
 
     db_driving_waiting_queue = models.DrivingWaitingQueue(
         player_id=driving_waiting_queue.player_id,
@@ -55,3 +57,69 @@ def create_driving_waiting_queue(db: Session, driving_waiting_queue: schemas.Dri
     db.commit()
     db.refresh(db_driving_waiting_queue)
     return db_driving_waiting_queue
+
+
+def get_driving_queue_item(db: Session, player_id: int):
+    return db.query(models.DrivingWaitingQueue) \
+        .where(models.DrivingWaitingQueue.player_id == player_id).first()
+
+
+"""
+    Will move player "to_move.player" so that it is after "before_item.player".
+"""
+def move_player_after_an_other_in_waiting_queue(db: Session, to_move: models.DrivingWaitingQueue,
+                                                before_item: models.DrivingWaitingQueue):
+    old_rank = to_move.rank
+
+    # Find the first queued element after the player, the one that is the next after him
+    existing_player_after = db.query(models.DrivingWaitingQueue).filter(models.DrivingWaitingQueue.rank > before_item.rank)\
+        .order_by(asc(models.DrivingWaitingQueue.rank))\
+        .limit(1).first()
+
+    if existing_player_after:
+        if existing_player_after.player_id == to_move.player_id:  # No need to move it, it's already after
+            return to_move
+
+        delta = existing_player_after.rank - before_item.rank
+        if delta > 0:
+            to_move.rank = before_item.rank + delta // 2
+        else:
+            raise ValueError('Impossible to rank this player no values left')
+    else:  # No one after the referenced player, simply putting our player here with the RANKING_STEP
+        to_move.rank = before_item.rank + RANKING_STEP
+
+    db.commit()
+    print(f'Moved {to_move.player.player_pseudo} from rank {old_rank} to rank {to_move.rank}'
+          f'so that he is after {before_item.player.player_pseudo} ({before_item.rank})')
+    return to_move
+
+
+"""
+    Will move player "to_move.player" so that it is before "after_item.player".
+"""
+def move_player_before_an_other_in_waiting_queue(db: Session, to_move: models.DrivingWaitingQueue,
+                                                after_item: models.DrivingWaitingQueue):
+    old_rank = to_move.rank
+
+    # Find the first queued element after the player, the one that is the next after him
+    existing_player_before = db.query(models.DrivingWaitingQueue).filter(
+        models.DrivingWaitingQueue.rank < after_item.rank) \
+        .order_by(desc(models.DrivingWaitingQueue.rank)) \
+        .limit(1).first()
+
+    if existing_player_before:
+        if existing_player_before.player_id == to_move.player_id:  # No need to move it, it's already after
+            return to_move
+
+        delta = after_item.rank - existing_player_before.rank
+        if delta > 0:
+            to_move.rank = after_item.rank - delta // 2
+        else:
+            raise ValueError('Impossible to rank this player no values left')
+    else:  # No one after the referenced player, simply putting our player here with the RANKING_STEP
+        to_move.rank = after_item.rank // 2  # Let some place behind it and before it
+
+    db.commit()
+    print(f'Moved {to_move.player.player_pseudo} from rank {old_rank} to rank {to_move.rank}'
+          f'so that he is after {after_item.player.player_pseudo} ({after_item.rank})')
+    return to_move
--- backend/donkeycarmanager/main.py
+++ backend/donkeycarmanager/main.py
@@ -1,6 +1,7 @@
 from typing import List, Union
 
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Query, Body
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from donkeycarmanager import crud, models, schemas
@@ -9,6 +10,13 @@ from donkeycarmanager.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Dependency
@@ -51,18 +59,66 @@ def read_player(player_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/drivingWaitingQueue/", response_model=schemas.DrivingWaitingQueue)
-def create_player(driving_waiting_queue: schemas.DrivingWaitingQueueCreate, db: Session = Depends(get_db)):
+def create_player_in_driving_queue(driving_waiting_queue: schemas.DrivingWaitingQueueCreate, db: Session = Depends(get_db)):
     # TODO check player_id isn't already in the waiting queue
     return crud.create_driving_waiting_queue(db=db, driving_waiting_queue=driving_waiting_queue)
 
 
 @app.get("/drivingWaitingQueue/", response_model=List[schemas.DrivingWaitingQueue])
-def read_players(by_rank: bool = True, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_players_in_driving_queue(by_rank: bool = True, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     if by_rank:
         waiting_players = crud.get_driving_waiting_queue_by_rank(db, skip=skip, limit=limit)
     else:
         waiting_players = crud.get_driving_waiting_queue(db, skip=skip, limit=limit)
     return waiting_players
+
+
+@app.post("/drivingWaitingQueue/{player_id}/move_after", response_model=schemas.DrivingWaitingQueue,
+          description="Move Player (player_id) after player with after_player_id")
+def move_player_in_driving_queue(player_id: int,
+                                 after_player_id: int = Body(
+                                     default=...,
+                                     embed=True,
+                                     description="Let's take Toto and toto.player_id == player_id (path parameter),"
+                                                 "and Titi with titi.player_id == after_player_id. After this request "
+                                                 "Toto will be just after Titi in the waiting queue. Meaning that Titi "
+                                                 "will start before Toto."
+                                 ),
+                                 db: Session = Depends(get_db)):
+    to_move = crud.get_driving_queue_item(db, player_id)
+    if to_move is None:  # Guard ensure the player we want to move exists
+        raise HTTPException(status_code=404, detail=f"Player to be moved with id {player_id} "
+                                                    f"not found in driving waiting queue")
+    before_item = crud.get_driving_queue_item(db, after_player_id)
+    if before_item is None:  # Guard ensure the player we want to move exists
+        raise HTTPException(status_code=404, detail=f"Player with id {after_player_id}, to be after "
+                                                    f"not found in driving waiting queue")
+
+    return crud.move_player_after_an_other_in_waiting_queue(db, to_move, before_item)
+
+
+@app.post("/drivingWaitingQueue/{player_id}/move_before", response_model=schemas.DrivingWaitingQueue,
+          description="Move Player (player_id) before player with before_player_id")
+def move_player_in_driving_queue(player_id: int,
+                                 before_player_id: int = Body(
+                                     default=...,
+                                     embed=True,
+                                     description="Let's take Toto and toto.player_id == player_id (path parameter),"
+                                                 "and Titi with titi.player_id == before_player_id. After this request "
+                                                 "Toto will be just before Titi in the waiting queue. Meaning that Toto"
+                                                 " will start before Titi."
+                                 ),
+                                 db: Session = Depends(get_db)):
+    to_move = crud.get_driving_queue_item(db, player_id)
+    if to_move is None:  # Guard ensure the player we want to move exists
+        raise HTTPException(status_code=404, detail=f"Player to be moved with id {player_id} "
+                                                    f"not found in driving waiting queue")
+    after_item = crud.get_driving_queue_item(db, before_player_id)
+    if after_item is None:  # Guard ensure the player we want to move exists
+        raise HTTPException(status_code=404, detail=f"Player with id {before_player_id}, to be before "
+                                                    f"not found in driving waiting queue")
+
+    return crud.move_player_before_an_other_in_waiting_queue(db, to_move, after_item)
 
 
 def main():
