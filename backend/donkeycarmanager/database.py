7d71b5e19355b99e84c0eebe537b814fa7a08da2
Benjamin BERNARD <benjamin.bernard@benvii.com>
2022-09-26 23:40:33 +0200

    :wrench: [Backend] Fixes #219, increase database pool and overflow, multiply by 3

--- backend/donkeycarmanager/database.py
+++ backend/donkeycarmanager/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://donkeycarmanager:donkeycar@localhost/donkeycarmanager"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=15, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
